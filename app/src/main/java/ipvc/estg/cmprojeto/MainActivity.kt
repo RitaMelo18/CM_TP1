310b4ac1a043639f2c08e6d2ad3be7434b42b9a0
RitaMelo18 <meloana@ipvc.pt>
2021-03-10 13:47:18 +0000

    Página Principal e Página das Notas

--- app/src/main/java/ipvc/estg/cmprojeto/MainActivity.kt
+++ app/src/main/java/ipvc/estg/cmprojeto/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
 
